b69a9b2ccec1c3a291d5c22d49c68ed4299f344c
RageBoy152 <89703723+RageBoy152@users.noreply.github.com>
2023-05-01 05:09:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -519,7 +519,7 @@ async function showAddRef() {
     userId = discordData.id
 
     //checks user authority
-    publishAuth = checkAuth(publisher,userId)
+    publishAuth = checkAuth('publisher',userId)
     
     if (publishAuth == 'not authorized') {
         //displays err for incorrect authority
