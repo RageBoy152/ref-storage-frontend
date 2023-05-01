33174e8c585c7ee4c58517a0ef321300a1ddedca
RageBoy152 <89703723+RageBoy152@users.noreply.github.com>
2023-05-01 03:07:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -474,7 +474,6 @@ window.onload = () => {
         })
         .then(result => result.json())
         .then(response => {
-            console.log(response)
             document.getElementById('loggedInPfp').src = `https://cdn.discordapp.com/avatars/${response.id}/${response.avatar}?size=1024`
         })
         .catch(console.error);
