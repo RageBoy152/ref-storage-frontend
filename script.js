7104e8c95d39cf3b921f02eb64aec330ebd662dd
RageBoy152 <89703723+RageBoy152@users.noreply.github.com>
2023-05-04 09:26:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,14 +55,7 @@ async function loadAddRefForm() {
             `
             document.getElementById('add-ref-modal-footer').remove()
         }   else if (publishAuth == 'authorized') {
-                //const addRefAuthorisedContentRaw = await fetch('https://raw.githubusercontent.com/RageBoy152/ref-storage-frontend/main/modals/add-ref.html')
-                //console.log(addRefAuthorisedContentRaw)
-                //console.log(JSON.stringify(addRefAuthorisedContentRaw))
-                //const addRefAuthorisedContent = await addRefAuthorisedContentRaw.json()
-            
                 $('#add-ref-modal-body').load('modals/add-ref.html')
-            
-                //document.getElementById('add-ref-modal-body').innerHTML = addRefAuthorisedContent.html
                 document.getElementById('add-ref-modal-body').classList.add('authorised-to-add-ref')
                 document.getElementById('add-ref-modal-footer').innerHTML = `
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
