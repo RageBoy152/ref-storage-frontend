4a8288c760c687c68d00a1fa90f2f85a8cb54058
RageBoy152 <89703723+RageBoy152@users.noreply.github.com>
2023-05-04 09:55:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -552,7 +552,7 @@ window.onload = () => {
 };
 
 
-function submitAddRefForm() => {
+function submitAddRefForm() {
     'use strict'
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
